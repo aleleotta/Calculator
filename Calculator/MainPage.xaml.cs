ec8383eb9bdb8937d79921ef176d27ea6839f351
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-12-05 14:28:19 +0100

    .

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -10,16 +10,52 @@
         double result = 0;
         string currentNumber = "0";
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void ButtonNum_Clicked(object sender, EventArgs e)
         {
+            int var = ButtonId();
+            if (currentNumber == "0")
+            {
+                currentNumber = var.ToString();
+            }
+            else
+            {
+                currentNumber = currentNumber + var.ToString();
+            }
             string displayText = display.Text;
-            if (currentNumber == "0") {
-                currentNumber = "7";
-            }
-            else {
-                currentNumber = currentNumber + "7";
-            }
             display.Text = currentNumber;
+        }
+
+        private int ButtonId()
+        {
+            int var = 0;
+            if (button1.IsPressed) {
+                var = 1;
+            }
+            else if (button2.IsPressed) {
+                var = 2;
+            }
+            else if (button3.IsPressed) {
+                var = 3;
+            }
+            else if (button4.IsPressed) {
+                var = 4;
+            }
+            else if (button5.IsPressed) {
+                var = 5;
+            }
+            else if (button6.IsPressed) {
+                var = 6;
+            }
+            else if (button7.IsPressed) {
+                var = 7;
+            }
+            else if (button8.IsPressed) {
+                var = 8;
+            }
+            else if (button9.IsPressed) {
+                var = 9;
+            }
+            return var;
         }
 
         private void ButtonOps_Clicked(object sender, EventArgs e)

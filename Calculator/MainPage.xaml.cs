61b39dae0592d4940c33b5f508964d465cfb97e7
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-12-05 14:02:39 +0100

    .

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -7,5 +7,26 @@
             InitializeComponent();
         }
 
+        double result = 0;
+        string currentNumber = "0";
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string displayText = display.Text;
+            if (currentNumber == "0") {
+                currentNumber = "7";
+            }
+            else {
+                currentNumber = currentNumber + "7";
+            }
+            display.Text = currentNumber;
+        }
+
+        private void ButtonOps_Clicked(object sender, EventArgs e)
+        {
+            string displayText = display.Text;
+            
+        }
+
     }
 }

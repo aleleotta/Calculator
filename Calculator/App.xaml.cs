78569967745ec92acd1f9a9447e54eea8a69154e
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-12-12 13:27:18 +0100

    .

--- Calculator/App.xaml.cs
+++ Calculator/App.xaml.cs
@@ -8,5 +8,16 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            const int newHeight = 400;
+            const int newWidth = 300;
+            window.Height = newHeight;
+            window.Width = newWidth;
+            return window;
+        }
+
     }
 }
